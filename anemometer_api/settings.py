b6acdc2c12d123552c8496afc6a6c6a851314e8c
Ricsi <allili.hamid@gmail.com>
2025-06-24 22:06:09 +0200

    Authentication enabled

--- anemometer_api/settings.py
+++ anemometer_api/settings.py
@@ -125,9 +125,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    # 'DEFAULT_PERMISSION_CLASSES': (
-    #     'rest_framework.permissions.IsAuthenticated',
-    # ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
     'DEFAULT_THROTTLE_RATES': {
         'windreading_submit': '100/hour'
     }
